a5e5c95f2538e68192f291cae3149f5ac1472780
akzReekoh <achilles.dinglasan@p3ople4u.com>
2015-12-02 15:51:27 +0800

    REEKOH-574
    -Finished sending of data to Parse
    -Finihsed documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# parse-connector
-Parse Connector Plugin for the Reekoh IoT Platform.
+# Parse Connector
+[![Build Status](https://travis-ci.org/Reekoh/parse-connector.svg)](https://travis-ci.org/Reekoh/parse-connector)
+![Dependencies](https://img.shields.io/david/Reekoh/parse-connector.svg)
+![Dependencies](https://img.shields.io/david/dev/Reekoh/parse-connector.svg)
+![Built With](https://img.shields.io/badge/built%20with-gulp-red.svg)
+
+Parse Connector plugin for the Reekoh IoT Platform. Integrates a Reekoh Instance with Parse to send push notifications.
+
+## Description
+This plugin sends notifications from the Reekoh Instance Parse Push Notification service.
+
+## Configuration
+To configure this plugin a Parse account, application and push notification channel is needed in order to provide the following:
+
+1. Application ID - The Parse Application ID to use.
+2. JavaScirpt Key -  The Parse Application's corresponding JavaScript Key.
+
+Other Parameters:
+
+1. Default Channels - The default channel(s) to send notificaitons.
+2. Default Message -  The default message to send.
+
+These parameters are then injected to the plugin from the platform.
+
+## Sample input data
+```
+{
+    channels: ['test-channel1', 'test-channel2'], //(Array)
+    message: 'This is a test message from Parse Connector Plugin.'
+}
+```
---
+++ test/parse-connector.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const APP_ID = '5GwhgEyOkTX2ebfN8ULSh23HArd1BU8WNxt6NEQ7',
+    JAVASCRIPT_KEY = '8FEIc5Pq7BpCFDuiyLz59sYT88NzbLoXDuzoRzlQ';
+
+var cp     = require('child_process'),
+	assert = require('assert'),
+	connector;
+
+describe('Connector', function () {
+	this.slow(5000);
+
+	after('terminate child process', function () {
+        setTimeout(function(){
+            connector.kill('SIGKILL');
+        }, 5000);
+	});
+
+	describe('#spawn', function () {
+		it('should spawn a child process', function () {
+			assert.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
+		});
+	});
+
+	describe('#handShake', function () {
+		it('should notify the parent process when ready within 5 seconds', function (done) {
+			this.timeout(5000);
+
+			connector.on('message', function (message) {
+				if (message.type === 'ready')
+					done();
+			});
+
+			connector.send({
+				type: 'ready',
+				data: {
+					options: {
+						app_id: APP_ID,
+						javascript_key: JAVASCRIPT_KEY,
+                        default_channels : ['default-channel'],
+                        default_message : 'This is a default message.'
+					}
+				}
+			}, function (error) {
+				assert.ifError(error);
+			});
+		});
+	});
+
+	describe('#data', function (done) {
+		it('should process the data', function () {
+			connector.send({
+				type: 'data',
+				data: {
+					channels: ['test-channel1', 'test-channel2'],
+                    message: 'This is a test message from Parse Connector Plugin.'
+				}
+			}, done);
+		});
+	});
+});
